1e255247ae572aa3b02d0c85dfadd83e8993cd63
Sergio Dev <sergio-dev@outlook.com>
2022-03-04 16:17:45 +0100

    change dirname in server

--- api/app.js
+++ api/app.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
-app.use("/static", express.static(path.resolve("./", "frontend", "static")))
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("./", "frontend", "index.html"))
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
 })
 
 module.exports = app
